5b1bff0c0dc9d49d56f30f0d4adb7f40765b7020
Sorarittzx <kornz.valentine@gmail.com>
2017-10-28 15:35:23 +0700

    back to toolbar ok

--- app/src/main/java/app/chawapee/sorarittzx/sorarittzxservice/fragment/RegisterFragment.java
+++ app/src/main/java/app/chawapee/sorarittzx/sorarittzxservice/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package app.chawapee.sorarittzx.sorarittzxservice.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import app.chawapee.sorarittzx.sorarittzxservice.MainActivity;
 import app.chawapee.sorarittzx.sorarittzxservice.R;
 
 
@@ -15,6 +17,35 @@ import app.chawapee.sorarittzx.sorarittzxservice.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+       //Create toolbar
+        createToolbar();
+
+    }//Main Medthod
+
+    private void createToolbar() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).setTitle(getResources().getString(R.string.new_register));
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);// enable มองเห็นปุ้ม
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true); // event click
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+    }
 
     //alt + ins
     @Nullable
